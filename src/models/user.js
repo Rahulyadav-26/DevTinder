1fcef7bbd1f6e3687827ce7bb0e9ca25d6116c08
Rahul yadav <rahuly7974@gmail.com>
2025-08-14 21:53:13 +0530

    explore validation

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require('validator');
+
 
 const userSchema = new mongoose.Schema(
   {
@@ -14,6 +16,11 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
       lowercase: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+            throw new Error("Invalid email");
+        }
+      }
     },
     password: {
       type: String,
